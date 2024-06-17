c1bbf6c45d9a1b85abd647dbc6bf37fc2aa22e2e
saemyyy <skusuma@sunrise.ch>
2024-06-17 22:11:55 +0200

    Shortened the Code

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 function run() {
   const search = document.querySelector("#search").value;
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("API Response (For general information):", data); // Log the API response
-      document.querySelector("#pokemon-name").innerText = data.name;
-      document.querySelector("#pokedex-number").innerText = data.id;
-    });
-
+  fetch(`https://pokeapi.co/api/v2/pokemon/${search}`);
   fetch(`https://pokeapi.co/api/v2/pokemon-species/${search}`)
     .then((response) => response.json())
     .then((data) => {
+      console.log("API Response (For general information):", data); // Log the API response
       console.log(
         "API Response (For Pokédex):",
         data.flavor_text_entries.filter(
           (element) => element.language.name === "en"
         )
-      ); // Log the API response
+      );
       document.querySelector("#pokemon-name").innerText = data.name;
-    })
-    .catch((err) => console.error(err));
+      document.querySelector("#pokedex-number").innerText = data.id;
+      document.querySelector("#pokedex-description").innerText =
+        data.flavor_text_entries.filter(
+          (element) => element.language.name === "en"
+        );
+    });
 }
