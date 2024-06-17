0f089c506d6c4d9b7b0f81c897d748ce63ec5c0f
saemyyy <skusuma@sunrise.ch>
2024-06-17 21:19:04 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function run() {
+  const search = document.querySelector("#search").value;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("API Response (For general information):", data); // Log the API response
+      document.querySelector("#pokemon-name").innerText = data.name;
+      document.querySelector("#pokedex-number").innerText = data.id;
+    });
+
+  fetch(`https://pokeapi.co/api/v2/pokemon-species/${search}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("API Response (For Pokédex):", data.flavor_text_entries); // Log the API response
+      document.querySelector("#pokemon-name").innerText = data.name;
+    })
+    .catch((err) => console.error(err));
+}
